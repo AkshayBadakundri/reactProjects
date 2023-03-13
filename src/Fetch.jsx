90b490117d5012919f896cca230a2aa1404d05d0
AkshayBadakundri <126859204+AkshayBadakundri@users.noreply.github.com>
2023-03-13 08:53:55 +0530

    done

---
+++ src/Fetch.jsx
@@ -0,0 +1,214 @@
+import React from 'react'
+
+const Fetch = (props) => {
+  return (
+    <div className='maindata'>
+        <div className='data1'>
+            <img src={props.details[0].stdPhoto} alt="" />
+            <h1>{props.details[0].stdName}</h1>
+            <h2>{props.details[0].college}</h2>
+            <h2>{props.details[0].Qualification}</h2>
+            <h2>{props.details[0].location}</h2>
+            <h2>{props.details[0].branch}</h2>
+            <h2>{props.details[0].course}</h2>
+        </div>
+        <div className='data1'>
+            <img src={props.details[1].stdPhoto} alt="" />
+            <h1>{props.details[1].stdName}</h1>
+            <h2>{props.details[1].college}</h2>
+            <h2>{props.details[1].Qualification}</h2>
+            <h2>{props.details[1].location}</h2>
+            <h2>{props.details[1].branch}</h2>
+            <h2>{props.details[1].course}</h2>
+        </div>
+
+         <div className='data1'>
+            <img src={props.details[2].stdPhoto} alt="" />
+            <h1>{props.details[2].stdName}</h1>
+            <h2>{props.details[2].college}</h2>
+            <h2>{props.details[2].Qualification}</h2>
+            <h2>{props.details[2].location}</h2>
+            <h2>{props.details[2].branch}</h2>
+            <h2>{props.details[2].course}</h2>
+        </div>
+        <div className='data1'>
+            <img src={props.details[3].stdPhoto} alt="" />
+            <h1>{props.details[3].stdName}</h1>
+            <h2>{props.details[3].college}</h2>
+            <h2>{props.details[3].Qualification}</h2>
+            <h2>{props.details[3].location}</h2>
+            <h2>{props.details[3].branch}</h2>
+            <h2>{props.details[3].course}</h2>
+        </div>
+        <div className='data1'>
+            <img src={props.details[4].stdPhoto} alt="" />
+            <h1>{props.details[4].stdName}</h1>
+            <h2>{props.details[4].college}</h2>
+            <h2>{props.details[4].Qualification}</h2>
+            <h2>{props.details[4].location}</h2>
+            <h2>{props.details[4].branch}</h2>
+            <h2>{props.details[4].course}</h2>
+        </div>
+        <div className='data1'>
+            <img src={props.details[5].stdPhoto} alt="" />
+            <h1>{props.details[5].stdName}</h1>
+            <h2>{props.details[5].college}</h2>
+            <h2>{props.details[5].Qualification}</h2>
+            <h2>{props.details[5].location}</h2>
+            <h2>{props.details[5].branch}</h2>
+            <h2>{props.details[5].course}</h2>
+        </div>
+
+        <div className='data1'>
+            <img src={props.details[6].stdPhoto} alt="" />
+            <h1>{props.details[6].stdName}</h1>
+            <h2>{props.details[6].college}</h2>
+            <h2>{props.details[6].Qualification}</h2>
+            <h2>{props.details[6].location}</h2>
+            <h2>{props.details[6].branch}</h2>
+            <h2>{props.details[6].course}</h2>
+        </div>
+        <div className='data1'>
+            <img src={props.details[7].stdPhoto} alt="" />
+            <h1>{props.details[7].stdName}</h1>
+            <h2>{props.details[7].college}</h2>
+            <h2>{props.details[7].Qualification}</h2>
+            <h2>{props.details[7].location}</h2>
+            <h2>{props.details[7].branch}</h2>
+            <h2>{props.details[7].course}</h2>
+        </div>
+        
+        <div className='data1'>
+            <img src={props.details[8].stdPhoto} alt="" />
+            <h1>{props.details[8].stdName}</h1>
+            <h2>{props.details[8].college}</h2>
+            <h2>{props.details[8].Qualification}</h2>
+            <h2>{props.details[8].location}</h2>
+            <h2>{props.details[8].branch}</h2>
+            <h2>{props.details[8].course}</h2>
+        </div>
+
+        
+        <div className='data1'>
+            <img src={props.details[9].stdPhoto} alt="" />
+            <h1>{props.details[9].stdName}</h1>
+            <h2>{props.details[9].college}</h2>
+            <h2>{props.details[9].Qualification}</h2>
+            <h2>{props.details[9].location}</h2>
+            <h2>{props.details[9].branch}</h2>
+            <h2>{props.details[9].course}</h2>
+        </div>
+        
+        <div className='data1'>
+            <img src={props.details[10].stdPhoto} alt="" />
+            <h1>{props.details[10].stdName}</h1>
+            <h2>{props.details[10].college}</h2>
+            <h2>{props.details[10].Qualification}</h2>
+            <h2>{props.details[10].location}</h2>
+            <h2>{props.details[10].branch}</h2>
+            <h2>{props.details[10].course}</h2>
+        </div>
+            <div className='data1'>
+            <img src={props.details[11].stdPhoto} alt="" />
+            <h1>{props.details[11].stdName}</h1>
+            <h2>{props.details[11].college}</h2>
+            <h2>{props.details[11].Qualification}</h2>
+            <h2>{props.details[11].location}</h2>
+            <h2>{props.details[11].branch}</h2>
+            <h2>{props.details[11].course}</h2>
+        </div>
+        
+        <div className='data1'>
+            <img src={props.details[12].stdPhoto} alt="" />
+            <h1>{props.details[12].stdName}</h1>
+            <h2>{props.details[12].college}</h2>
+            <h2>{props.details[12].Qualification}</h2>
+            <h2>{props.details[12].location}</h2>
+            <h2>{props.details[12].branch}</h2>
+            <h2>{props.details[12].course}</h2>
+        </div>
+        
+        <div className='data1'>
+            <img src={props.details[13].stdPhoto} alt="" />
+            <h1>{props.details[13].stdName}</h1>
+            <h2>{props.details[13].college}</h2>
+            <h2>{props.details[13].Qualification}</h2>
+            <h2>{props.details[13].location}</h2>
+            <h2>{props.details[13].branch}</h2>
+            <h2>{props.details[13].course}</h2>
+        </div>
+        
+        {/* <div className='data1'>
+            <img src={props.details[14].stdPhoto} alt="" />
+            <h1>{props.details[14].stdName}</h1>
+            <h2>{props.details[14].college}</h2>
+            <h2>{props.details[14].Qualification}</h2>
+            <h2>{props.details[14].location}</h2>
+            <h2>{props.details[14].branch}</h2>
+            <h2>{props.details[14].course}</h2>
+        </div> */}
+         
+        <div className='data1'>
+            <img src={props.details[15].stdPhoto} alt="" />
+            <h1>{props.details[15].stdName}</h1>
+            <h2>{props.details[15].college}</h2>
+            <h2>{props.details[15].Qualification}</h2>
+            <h2>{props.details[15].location}</h2>
+            <h2>{props.details[15].branch}</h2>
+            <h2>{props.details[15].course}</h2>
+        </div>
+        
+        <div className='data1'>
+            <img src={props.details[16].stdPhoto} alt="" />
+            <h1>{props.details[16].stdName}</h1>
+            <h2>{props.details[16].college}</h2>
+            <h2>{props.details[16].Qualification}</h2>
+            <h2>{props.details[16].location}</h2>
+            <h2>{props.details[16].branch}</h2>
+            <h2>{props.details[16].course}</h2>
+        </div>
+   {/*     
+        <div className='data1'>
+            <img src={props.details[17].stdPhoto} alt="" />
+            <h1>{props.details[17].stdName}</h1>
+            <h2>{props.details[17].college}</h2>
+            <h2>{props.details[17].Qualification}</h2>
+            <h2>{props.details[17].location}</h2>
+            <h2>{props.details[17].branch}</h2>
+            <h2>{props.details[17].course}</h2>
+        </div>
+        
+        <div className='data1'>
+            <img src={props.details[18].stdPhoto} alt="" />
+            <h1>{props.details[18].stdName}</h1>
+            <h2>{props.details[18].college}</h2>
+            <h2>{props.details[18].Qualification}</h2>
+            <h2>{props.details[18].location}</h2>
+            <h2>{props.details[18].branch}</h2>
+            <h2>{props.details[18].course}</h2>
+        </div>
+        
+        <div className='data1'>
+            <img src={props.details[19].stdPhoto} alt="" />
+            <h1>{props.details[19].stdName}</h1>
+            <h2>{props.details[19].college}</h2>
+            <h2>{props.details[19].Qualification}</h2>
+            <h2>{props.details[19].location}</h2>
+            <h2>{props.details[19].branch}</h2>
+            <h2>{props.details[19].course}</h2>
+        </div> 
+        
+         <div className='data1'>
+            <img src={props.details[20].stdPhoto} alt="" />
+            <h1>{props.details[20].stdName}</h1>
+            <h2>{props.details[20].college}</h2>
+            <h2>{props.details[20].Qualification}</h2>
+            <h2>{props.details[20].location}</h2>
+            <h2>{props.details[20].branch}</h2>
+            <h2>{props.details[20].course}</h2>
+        </div>*/}
+    </div>
+  )
+}
+
+export default Fetch
