6d32fa8a68f1830e0fcabbf46e14ef76304ffc3a
AkshayBadakundri <126859204+AkshayBadakundri@users.noreply.github.com>
2023-03-15 23:49:47 +0530

    done

--- src/statesPrg/App.jsx
+++ src/statesPrg/App.jsx
@@ -1,18 +1,72 @@
 
-import JSON from "./statesPrg/users.json"
-import Table from './statesPrg/Table.jsx';
   import React, { Component } from 'react';
-  
-  class App extends Component {
+import './counter.css'
+  // class App extends Component {
+  //   constructor()
+  //   {
+  //     super()
+  //     this.state={json:JSON}
+  //   }
+  //   render() {
+  //     return (
+  //       <div>
+  //         <Table data={this.state.json}/>
+  //       </div>
+  //     )
+  //   }
+  // }
+
+  //  class App extends Component {
+
+  //   constructor()
+  //   {
+  //     super()
+  //     this.state={
+  //       username:"akshay"
+  //     }
+  //   }
+  //   clickBtn=()=>{
+  //     this.setState({username:"appu"})
+  //   }
+  //   render() {
+  //     return (
+  //       <div>
+  //         <h1>{this.state.username}</h1>
+  //         <button onClick={this.clickBtn}>change name</button>
+  //       </div>
+  //     )
+  //   }
+  // }
+ class App extends Component {
+
     constructor()
     {
       super()
-      this.state={json:JSON}
+      this.state={
+        count:0
+      }
+    }
+
+    handelIncrement=()=>{
+      this.setState({count:this.state.count+1})
+    }
+
+    
+    handelDecrement=()=>{
+      this.setState({count:this.state.count-1})
+    }
+    handelReset=()=>{
+      this.setState({count:0})
     }
     render() {
       return (
-        <div>
-          <Table data={this.state.json}/>
+        <div className="div11">
+          <h1>{this.state.count}</h1>
+          <div className="divbtn22">
+          <button onClick={this.handelIncrement}>+Increment</button>
+          <button onClick={this.handelDecrement}>-Decrement</button>
+          <button className='reset' onClick={this.handelReset}>RESET</button>
+          </div>
         </div>
       )
     }
